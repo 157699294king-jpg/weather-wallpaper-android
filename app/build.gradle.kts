95da95ba1e37ca10cd90e23e4ec1b892e36a4d11
157699294king-jpg <157699294king@gmail.com>
2026-08-31 16:41:10 +0800

    Release futuristic UI v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
         applicationId = "com.weather.wallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 }
