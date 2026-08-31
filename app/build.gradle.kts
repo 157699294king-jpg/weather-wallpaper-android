1eb687a27ffa8ee50b2ad9ce63d49c3a1d0e6280
157699294king-jpg <157699294king@gmail.com>
2026-08-31 16:28:04 +0800

    Add app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.weather.wallpaper"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.weather.wallpaper"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
