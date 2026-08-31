c8bb488c00c6f28f8895a369d33672cdf577cbce
157699294king-jpg <157699294king@gmail.com>
2026-08-31 16:52:28 +0800

    Release default wallpaper and background sync v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
         applicationId = "com.weather.wallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 }
